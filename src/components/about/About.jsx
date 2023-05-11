67d6c1f1fe0274a2ce7fcce54b95eb3e870c581b
samarQaddour <samarqaddour051@gmail.com>
2023-05-11 01:36:47 -0700

    create about and style of it start create experience section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,43 @@
 import React from "react";
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward,FaUsers} from 'react-icons/fa'
+import {AiTwotoneFolderOpen} from 'react-icons/ai'
 const About = () => {
     return(
         <section id='about'>
-            About
+            <h5>Get To Know</h5>
+            <h2>About Me</h2>
+            <div className='container container__about'>
+                <div className='about__me'>
+                    <div className='about__me__image'>
+                        <img src={ME} alt=''/>
+                    </div>
+                </div>
+                <div className='about__content'>
+                    <div className='about__cards'>
+                        <article className='about__card'>
+                         <FaAward className='about__icon'/>
+                         <h5>Experience</h5>
+                            <small>1+ Years working</small>
+                        </article>
+                        <article className='about__card'>
+                         <FaUsers className='about__icon'/>
+                         <h5>Client</h5>
+                            <small>+30 worldwide</small>
+                        </article>
+                        <article className='about__card'>
+                         <AiTwotoneFolderOpen className='about__icon'/>
+                         <h5>Experience</h5>
+                            <small>1 Years working</small>
+                        </article>
+                    </div>
+                    <p>Lorem ipsum is placeholder text commonly
+                        used in the graphic, print, and publishing industries
+                        for previewing layouts and visual mockups.</p>
+                    <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+                </div>
+            </div>
         </section>
     )
 }
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -6,14 +6,14 @@ import {BsBookmarkStarFill} from 'react-icons/bs'
 import {RiServiceLine} from 'react-icons/ri'
 import {useState} from "react";
 const Nav = () => {
-    const [navActive,setNavActive]=useState('#')
+    const [activeNav,setactiveNav]=useState('#')
     return(
       <nav>
-          <a href="/#" onClick={() => setNavActive('#')} className={navActive==='#'? 'active' : ''}><AiOutlineHome/></a>
-          <a href="#about" onClick={() => setNavActive("#about")} className={navActive === "#about" ? 'active' : ''}><FaUserAlt/></a>
-          <a href="#experience" onClick={() => setNavActive('#experience')} className={navActive === '#experience' ? 'active' : ''} ><BsBookmarkStarFill/></a>
-          <a href="#service" onClick={() => setNavActive('#service')} className={navActive === '#service' ? 'active' : ''}><RiServiceLine/></a>
-          <a href="#contact" onClick={() => setNavActive('#contact')} className={navActive === '#contact' ? 'active' : ''}><AiFillMessage/></a>
+          <a href="/#" onClick={() => setactiveNav('#')} className={activeNav==='#'? 'active' : ''}><AiOutlineHome/></a>
+          <a href="#about" onClick={() => setactiveNav("#about")} className={activeNav === "#about" ? 'active' : ''}><FaUserAlt/></a>
+          <a href="#experience" onClick={() => setactiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''} ><BsBookmarkStarFill/></a>
+          <a href="#service" onClick={() => setactiveNav('#service')} className={activeNav === '#service' ? 'active' : ''}><RiServiceLine/></a>
+          <a href="#contact" onClick={() => setactiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiFillMessage/></a>
       </nav>
     )
 }
