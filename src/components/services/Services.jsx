5245a998823f5df84f7be97028be08010d57ae06
samarQaddour <samarqaddour051@gmail.com>
2023-05-12 11:15:05 -0700

    experience remove border radius from class me

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,118 @@
 import React from "react";
 import './Services.css'
+import {BiCheck} from "react-icons/bi";
 const Services = () => {
     return(
         <section id='service'>
-            service
+          <h5>What I offer</h5>
+            <h2>Services</h2>
+            <div className='container services__container'>
+                <article className='service'>
+                 <div className='service__head'>
+                     <h3>React Project</h3>
+                 </div>
+                    <ul>
+                     <div className='service__list'>
+                         <li>
+                             <BiCheck className='service__list__icon' size={50}/>
+                             <p> used in the graphic, print, and publishing industries
+                                 for previewing layouts and visual mockups.</p>
+                         </li>
+                     </div>
+                        <div className='service__list'>
+                         <li>
+                             <BiCheck className='service__list__icon' size={50}/>
+                             <p> used in the graphic, print, and publishing industries
+                                 for previewing layouts and visual mockups.</p>
+                         </li>
+                     </div>
+                        <div className='service__list'>
+                         <li>
+                             <BiCheck className='service__list__icon' size={50}/>
+                             <p> used in the graphic, print, and publishing industries
+                                 for previewing layouts and visual mockups.</p>
+                         </li>
+                     </div>
+                        <div className='service__list'>
+                         <li>
+                             <BiCheck className='service__list__icon' size={50}/>
+                             <p> used in the graphic, print, and publishing industries
+                                 for previewing layouts and visual mockups.</p>
+                         </li>
+                     </div>
+                    </ul>
+                </article>
+                <article className='service'>
+                 <div className='service__head'>
+                     <h3>React Project</h3>
+                 </div>
+                    <ul>
+                        <div className='service__list'>
+                            <li>
+                                <BiCheck className='service__list__icon' size={50}/>
+                                <p> used in the graphic, print, and publishing industries
+                                    for previewing layouts and visual mockups.</p>
+                            </li>
+                        </div>
+                        <div className='service__list'>
+                            <li>
+                                <BiCheck className='service__list__icon' size={50}/>
+                                <p> used in the graphic, print, and publishing industries
+                                    for previewing layouts and visual mockups.</p>
+                            </li>
+                        </div>
+                        <div className='service__list'>
+                            <li>
+                                <BiCheck className='service__list__icon' size={50}/>
+                                <p> used in the graphic, print, and publishing industries
+                                    for previewing layouts and visual mockups.</p>
+                            </li>
+                        </div>
+                        <div className='service__list'>
+                            <li>
+                                <BiCheck className='service__list__icon' size={50}/>
+                                <p> used in the graphic, print, and publishing industries
+                                    for previewing layouts and visual mockups.</p>
+                            </li>
+                        </div>
+                    </ul>
+                </article>
+                <article className='service'>
+                 <div className='service__head'>
+                     <h3>React Project</h3>
+                 </div>
+                    <ul>
+                        <div className='service__list'>
+                            <li>
+                                <BiCheck className='service__list__icon' size={50}/>
+                                <p> used in the graphic, print, and publishing industries
+                                    for previewing layouts and visual mockups.</p>
+                            </li>
+                        </div>
+                        <div className='service__list'>
+                            <li>
+                                <BiCheck className='service__list__icon' size={50}/>
+                                <p> used in the graphic, print, and publishing industries
+                                    for previewing layouts and visual mockups.</p>
+                            </li>
+                        </div>
+                        <div className='service__list'>
+                            <li>
+                                <BiCheck className='service__list__icon' size={50}/>
+                                <p> used in the graphic, print, and publishing industries
+                                    for previewing layouts and visual mockups.</p>
+                            </li>
+                        </div>
+                        <div className='service__list'>
+                            <li>
+                                <BiCheck className='service__list__icon' size={50}/>
+                                <p> used in the graphic, print, and publishing industries
+                                    for previewing layouts and visual mockups.</p>
+                            </li>
+                        </div>
+                    </ul>
+                </article>
+            </div>
         </section>
     )
 }
