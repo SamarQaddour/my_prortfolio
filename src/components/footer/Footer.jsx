4212fa6f3704e573d1e768f7ec0677277d6d42b5
samarQaddour <samarqaddour051@gmail.com>
2023-05-15 16:38:15 -0700

    footer complete

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,8 +6,7 @@ import {FiInstagram} from 'react-icons/fi';
 const Footer = () => {
     return(
       <footer>
-          <a href='/#' className='footer
-          --logo'>Samar</a>
+          <a href='/#' className='footer--logo'>Samar</a>
           <ul className='permalinks'>
              <li><a href='/#'>Home</a></li>
              <li><a href='#about'>About</a></li>
@@ -18,9 +17,12 @@ const Footer = () => {
              <li><a href='#contact'>contact</a></li>
           </ul>
           <div className='footer__socials'>
-              <a href='https://facebook.com'><FaFacebookF/></a>
-              <a href='https://instagram.com'><FiInstagram/></a>
-              <a href='https://twitter.com'><IoLogoTwitter/></a>
+              <a href='https://facebook.com'><FaFacebookF size={35}/></a>
+              <a href='https://instagram.com'><FiInstagram size={35}/></a>
+              <a href='https://twitter.com'><IoLogoTwitter size={35}/></a>
+          </div>
+          <div className='footer__copyright'>
+              <small>&copy; SAMAR Tutorials.All rights reservice</small>
           </div>
       </footer>
     )
